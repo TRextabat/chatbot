95944c298bcf5a77cb34a72c7e33722770ad824d
Trex <amiramiritabat01@gmail.com>
2024-10-17 15:59:07 +0300

    Change display  fields

--- chatbot/pdf/admin.py
+++ chatbot/pdf/admin.py
@@ -4,7 +4,7 @@ from .models import PDFDocument, PDFVectorEmbedding
 
 @admin.register(PDFDocument)
 class PDFDocumentAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created', 'modified', 'status', 'status_changed')
+    list_display = ('title', 'created', 'modified', 'status')
     search_fields = ('title', 'description')
-    list_filter = ('status', 'created', 'modified', 'status_changed')
-    readonly_fields = ('id', 'parsed_text', 'created', 'modified', 'status_changed')
+    list_filter = ('status', 'created', 'modified')
+    readonly_fields = ('id', 'parsed_text', 'created', 'modified')
