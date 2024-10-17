ca5351cbb552933d03134ee30003f4023fb2f2a5
Trex <amiramiritabat01@gmail.com>
2024-10-17 16:01:30 +0300

    ADD pdfview url

--- chatbot/chatbot/urls.py
+++ chatbot/chatbot/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.urls import path
 from django.contrib import admin
 from rest_framework import routers
+from pdf.views import PDF
 
 router = routers.DefaultRouter()
 
@@ -23,4 +24,6 @@ urlpatterns = router.urls
 
 urlpatterns += [
     path('admin/', admin.site.urls),
+    path('pdf/', PDF.as_view())
+    
 ]
