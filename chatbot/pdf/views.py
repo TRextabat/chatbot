8a11678b20a68a7547b3bdec30e1f0d447e73374
Trex <amiramiritabat01@gmail.com>
2024-10-17 16:00:44 +0300

    ADD post endpoint

--- chatbot/pdf/views.py
+++ chatbot/pdf/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from rest_framework import views, status
+from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+from django.core.files.uploadhandler import TemporaryFileUploadHandler
+from .models import PDFDocument
+from .serializers import PDFDocumentSerializer
+import logging
 
-# Create your views here.
+logger = logging.getLogger(__name__)
+
+class PDFDocumentPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+
+class PDF(views.APIView):
+    
+    def post(self, request):
+
+        try:
+            request.upload_handlers.insert(0, TemporaryFileUploadHandler(request=request))
+            serializer = PDFDocumentSerializer(data=request.data)
+
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.title, status=status.HTTP_201_CREATED)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            logger.error(e)
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={"error": str(e)})
+
