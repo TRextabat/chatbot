fcd8fd7dc7547c1cf249012961e289d4d8cb18df
Trex <amiramiritabat01@gmail.com>
2024-10-18 21:20:25 +0300

    DEBUG change pdf urlname

--- chatbot/chatbot/urls.py
+++ chatbot/chatbot/urls.py
@@ -39,7 +39,7 @@ urlpatterns = router.urls
 
 urlpatterns += [
     path('admin/', admin.site.urls),
-    path('pdf/', PDF.as_view(), name='pdf-up'),
+    path('pdf/', PDF.as_view(), name='pdf'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
