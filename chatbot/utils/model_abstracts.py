4aec3a32eda3203b3e41dcb3d3e4f8e2cf589688
Trex <amiramiritabat01@gmail.com>
2024-10-27 15:56:03 +0300

    ADD DocString

--- chatbot/utils/model_abstracts.py
+++ chatbot/utils/model_abstracts.py
@@ -4,8 +4,15 @@ from django.db import models
 
 class Model(models.Model):
     """
-    Abstract model to add UUID as primary key
+    Abstract base model class with a UUID primary key.
+
+    Attributes:
+        id (UUIDField): Primary key for the model, automatically generated using UUID4.
+
+    Meta:
+        abstract (bool): Indicates that this is an abstract base class and should not be used to create any database table.
     """
+
     id = models.UUIDField(primary_key=True, 
                           default=uuid.uuid4, 
                           editable=False)
