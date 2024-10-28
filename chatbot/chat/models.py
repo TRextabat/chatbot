d3e8543f033fc05c9b68f11b4ab8dabe97505f94
Trex <amiramiritabat01@gmail.com>
2024-10-28 17:52:24 +0300

    ADD session and masssagr models

--- chatbot/chat/models.py
+++ chatbot/chat/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django_extensions.db.models import TimeStampedModel, TitleDescriptionModel, ActivatorModel
+from utils.model_abstracts import Model
 
-# Create your models here.
+class ChatSession(Model,
+                  TimeStampedModel,
+                  ActivatorModel,
+                  TitleDescriptionModel
+                  ):
+    
+    is_active = models.BooleanField(default=True, help_text="Indicates if the session is active")
+    pdf_documents = models.ManyToManyField('pdf.PDFDocument', related_name="chat_sessions")
+    
+    class Meta:
+        indexes = [
+            models.Index(fields=["is_active", "modified"]),
+        ]
+
+    def __str__(self):
+        return f"ChatSession: {self.title} (ID: {self.id})"
+
+
+class ChatMassage(TimeStampedModel):
+
+    session = models.ForeignKey(ChatSession, on_delete=models.CASCADE, related_name="messages")
+    message_text = models.TextField(help_text="The content of the message.")
+    is_bot_response = models.BooleanField(default=False, help_text="True if this is a bot response")
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["session", "created"]),
+        ]
+    
+    def __str__(self):
+        return f"ChatMessage by {self.sender} in Session ID: {self.session.id}"
